5381eb5d73b168b90eb4879911bc1e3cb24acdd3
Riley Boyd <contact@rileyboyd.com>
2017-12-07 14:14:21 -0500

    Data types, numbers and strings

---
+++ ch03/Numbers/script.js
@@ -0,0 +1,13 @@
+var a;
+a = 5;
+a = 1000000;
+a = 123.564;
+a = -500;
+alert(a);
+
+var b = 123;
+var c = "123";
+
+/* Strings */
+// JS allows you to use single quotes or double quotes to define a string.
+//Traditionally, other languages restrict you to double quotes, so it can be wise to use double quotes, for convention and easy switching between languages.
