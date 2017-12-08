3a0e2a6318a3a66db7b379626558a7f6c3cc559b
Riley Boyd <contact@rileyboyd.com>
2017-12-08 13:52:09 -0500

    Chapter 3 end: Commenting

--- ch03/Comments/script.js
+++ ch03/Comments/script.js
@@ -6,3 +6,16 @@ var z = false;
 var name = prompt("What is your name?");
 var message = "Hello, ";
 alert(message + name);
+
+// Comments in javascript
+
+// This is a "single-line comment", typical of what one finds in a C-style language
+var a; // A single line comment can also go after a statement.
+
+/*
+
+You can also do a multi-line comment this way.
+
+Good for commenting out large chunks of code, can use the single-line format otherwise.
+
+*/
