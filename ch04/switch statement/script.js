40473f4fb38a528176171d3b3a030df5b8a3bb66
Riley Boyd <contact@rileyboyd.com>
2017-12-08 15:20:23 -0500

    Chapter 4: Switch statements, chapter 4 complete

--- ch04/switch statement/script.js
+++ ch04/switch statement/script.js
@@ -1,5 +1,6 @@
 var grade = "Premium";
 
+/*
 if ( grade === "Regular") {
      alert("It's $3.15");
 }
@@ -9,3 +10,46 @@ if ( grade === "Premium") {
 if ( grade === "Diesel") {
      alert("It's $3.47");
 }
+*/
+
+// Switch statements -- very useful in situations where you have a range of set values to check against.
+/* 
+//My first shot at it
+switch (grade) {
+	
+	case: "Regular" {
+		alert("its reg");
+	}
+	break;
+
+	case "Premium": alert("its prem");
+	break;
+
+	case "Diesel": alert("Its dies");
+	break;
+
+	default: alert("none of the above");
+
+}
+*/
+
+switch (grade) {
+	
+	case "Regular":
+		alert("its reg");
+		break;
+
+	case "Premium":
+		alert("its prem");
+		break;
+
+	case "Diesel":
+		alert("Its Diesel");
+		break;
+
+	default:
+		alert("none of the above");
+
+}
+
+/* Because of "fallthrough", as I read in my pro JS book, we must add "break" to prevent it from falling through. */
