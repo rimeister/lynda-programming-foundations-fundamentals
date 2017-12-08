10ee262df15f0aeded479a3473177345920b60be
Riley Boyd <contact@rileyboyd.com>
2017-12-08 12:34:53 -0500

    Chapter 3: Whitespace, operators

--- ch03/Numbers/script.js
+++ ch03/Numbers/script.js
@@ -1,4 +1,4 @@
-var a;
+/*var a;
 a = 5;
 a = 1000000;
 a = 123.564;
@@ -7,7 +7,31 @@ alert(a);
 
 var b = 123;
 var c = "123";
+*/
 
 /* Strings */
 // JS allows you to use single quotes or double quotes to define a string.
 //Traditionally, other languages restrict you to double quotes, so it can be wise to use double quotes, for convention and easy switching between languages.
+
+/* Operators */
+/* Like any programming language, Javascript has operator precedence. Some operators are treated as more important than others. BEDMAS, I believe. */
+// You can make sure certain operations are done first by surrounding them with parentheses (i.e., brackets). */
+/*
+/ B -- Brackets
+/ E -- Exponents
+/ D -- Division
+/ M -- Multiplication
+/ A -- Addition
+/ S -- Subtraction
+*/
+
+var score = 100;
+score = score + 10; // 110
+score += 10; // 120
+
+var a = 1; // 1
+a += 1; // 2
+a++; // 3
+console.log("Score value is " + score + "\na value is " +a);
+
+// This +1 assignment operator is where C++ got its name from: It's the "C" language, "plus one", the next version of C.
--- ch03/Whitespace/script.js
+++ ch03/Whitespace/script.js
@@ -1,11 +1,14 @@
-var a = 5;
-var b = 10;
-var c = 20;
-a ++ ;
-b--;
-c += 10;
-var x = true;
-var z = false;
-var name = prompt("What is your name?");
-var message = "Hello, ";
-alert(message + name);
+// var a = 5;
+// var b = 10;
+// var c = 20;
+// a ++ ;
+// b--;
+// c += 10;
+// var x = true;
+// var z = false;
+// var name = prompt("What is your name?");
+// var message = "Hello, ";
+// alert(message + name);
+
+// Javascript, like all modern programming languages, does not care about whitespace: i.e., spaces, tabs, and line breaks. We use the semicolon to tell javascript where the lines end, instead of linebreaks.
+// Javascript, and other C-based lanugages, use the semicolon right after the value at the end of a statement.
