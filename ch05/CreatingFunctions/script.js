660885e520d8c7f29745781648284186ccb03d70
Riley Boyd <rboyd@kpd-i.com>
2017-12-11 11:07:28 -0500

    Chapter 5: Functions, passing in information via arguments/parameters, passing information out via 'return'.

--- ch05/CreatingFunctions/script.js
+++ ch05/CreatingFunctions/script.js
@@ -1,8 +1,13 @@
-var a = 5;
-var b = 10;
-var c = 20;
-var d = a + b + c;
-alert("The value of d is: " + d );
+// Good function naming connvention: verb + noun format: verbNoun(); e.g., animateBird();, calculateScore();
 
+function sumIntegers() {
+	var a = 5;
+	var b = 10;
+	var c = 20;
+	var d = a + b + c;
+	alert("The value of d is: " + d );
+}
+
+sumIntegers();
 
 
--- ch05/Parameters/script.js
+++ ch05/Parameters/script.js
@@ -1,8 +1,25 @@
-function addTwoNumbers() {
-   	var a = 5;
-	var b = 10;
+function addTwoNumbers(a,b) {
 	var result = a + b;
-	alert(result);
+	return result;
 }
 
-addTwoNumbers();
+var summedVal = addTwoNumbers(5,10);
+
+alert(summedVal);
+
+/* Interesting distinction:
+	"Parameters" refers to the variable names in the function declaration -- i.e.,
+	(a,b).
+	"Arguments" refers to the the values you pass into the function -- e.g., 
+	(5,10).
+*/
+
+/*
+
+	Interesting way of thinking:
+	Arguments pass information INTO a function.
+	"Return" passes information back OUT of a function.
+
+	Return exits the function.
+
+*/
