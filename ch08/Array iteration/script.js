ba9a93779165ca620eff117eff233d373693e5d0
Riley Boyd <rboyd@kpd-i.com>
2017-12-12 12:15:28 -0500

    Chapter 8 done. Did exercise in which I iterated over an array of numbers, and added all the  numbers together. Put the for loop into a function so I could call it on a button click, added conditional to check that the collection (i.e., array) hadn't already been summed.

--- ch08/Array iteration/script.js
+++ ch08/Array iteration/script.js
@@ -1,12 +1,30 @@
-var myArray = [500,500,500,500,500];
+var myArray = [500,500,500,500,500,120,800];
 
 var total = 0;
 
-for ( var i = 0 ; i < myArray.length ; i++ ) {
-    // add the current element to the total	
-    total = total + myArray[i];
-}
+function sumCollection(valCollection) {
+
+	if ( total < 1 ) {
+
+		for ( var i = 0 ; i < valCollection.length ; i++ ) {
+		    // add the current element to the total	
+		    total = total + valCollection[i];
+		}
+
+	} 
+
+} 
+
 
 // after we're done with the loop
-alert("The total is: " + total);
+document.getElementById('seeTotalVal').addEventListener('click', function(event){
+	event.preventDefault();
+	alert(total);
+});
+
+document.getElementById('sumBtn').addEventListener('click', function(event){
+	event.preventDefault();
+	sumCollection(myArray);
+	alert("The numbers have been added together");
+});
 
