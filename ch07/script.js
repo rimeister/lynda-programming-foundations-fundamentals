3cac7168525c08e75c1faa5447264b8b2be29548
Riley Boyd <rboyd@kpd-i.com>
2017-12-12 10:41:42 -0500

    Chapter 7: made a script to slice a word from a page and replace it on the page

--- ch07/script.js
+++ ch07/script.js
@@ -10,3 +10,24 @@ document.getElementById('testBtn').addEventListener('click', function(event){
 		alert("The value is a number");
 	}
 })
+
+var keyPhraseEle = document.getElementById('keyPhrase');
+var phrase = keyPhraseEle.innerHTML;
+var slicedWord;
+var newPhrase;
+
+document.getElementById('findGroovy').addEventListener('click', function(event){
+	event.preventDefault();
+
+	if (phrase.indexOf('groovy') > -1) {
+		alert('Groovy can be found at index ' + phrase.indexOf('groovy'));
+		slicedWord = phrase.slice(10,17);
+
+	} else {
+		alert('"' + slicedWord + '" has been removed from the phrase');
+	}
+
+	newPhrase = phrase.replace(slicedWord,'');
+	keyPhraseEle.innerHTML = newPhrase;
+
+});
